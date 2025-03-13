5c5d7ba0c3a5896863b7342c6da47fec667e905e
Maksim Narushevich <maxim.narushevich.ext@bark.com>
2025-03-13 23:32:20 +0700

    AL-5: Removed unused code

--- src/AuditLogsServiceProvider.php
+++ src/AuditLogsServiceProvider.php
@@ -4,38 +4,19 @@ declare(strict_types=1);
 
 namespace MNarushevich\AuditLogs;
 
-use Aws\DynamoDb\DynamoDbClient;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
-use Mnarushevich\AuditLogs\Services\AuditLogs;
 
 class AuditLogsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/auditlogs.php', 'auditlogs');
-
-        $this->app->singleton('auditlogs', function () {
-            return new AuditLogs();
-        });
-
-        $this->app->singleton(DynamoDbClient::class,  function () {
-            return new DynamoDbClient([
-                'version' => 'latest',
-                'region' => Config::get('auditlogs.dynamodb.region'),
-                'credentials' => [
-                    'key' => Config::get('auditlogs.dynamodb.key'),
-                    'secret' => Config::get('auditlogs.dynamodb.secret'),
-                ],
-                'endpoint' => Config::get('auditlogs.dynamodb.endpoint'),
-            ]);
-        });
+        $this->mergeConfigFrom(__DIR__.'/../config/audit_logs.php', 'audit_logs');
     }
 
     public function boot(): void
     {
         $this->publishes([
-            __DIR__.'/../config/auditlogs.php' => config_path('auditlogs.php'),
+            __DIR__.'/../config/audit_logs.php' => config_path('audit_logs.php'),
         ], 'config');
         $this->publishesMigrations([
             __DIR__.'/../database/migrations' => database_path('migrations'),
